20e1594aedf28b1efe2557d31ca4b9e7ee415424
zubeneschamali <tufengzhi@gmail.com>
2018-01-18 20:09:36 +0800

    Fix Bugs

--- cifar10/filters.py
+++ cifar10/filters.py
@@ -2,17 +2,18 @@ import cv2
 import numpy as np
 
 def filter1(img):
+	img = img[i].numpy()
 	for i in range(img.size()[0]):
-		print(img.size())
-		img2 = img[i].numpy()
+		img2 = img[i]
 		hist, bins = np.histogram(img2.flatten(), 256, [0,256])
 		cdf = hist.cumsum()
 		cdf_normalized = cdf * hist.max() / cdf.max()
 		cdf_m = np.ma.masked_equal(cdf, 0)
 		cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
 		cdf = np.ma.filled(cdf_m, 0).astype('uint8')
-		img[i] = torch.from_numpy(cdf[img2])
-		return img
+		img[i] = cdf[img2]
+	img = torch.from_numpy(img)
+	return img
 
 def filter2(img, size):
 	img2 = cv2.blur(img, (size, size))
